a67b7b19b4ed99e34e6620cbe015cbe57b2264d5
Arc <arky.happ@gmail.com>
2020-05-28 23:01:42 +0300

    0.1.0

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Note app with React and react-hooks</h1>
+        <p className="lead">
+          App version <strong>0.1.0</strong>
+        </p>
+      </div>
+    </div>
+  )
